aa8e488d517078aca3995201b22354c513d4b955
YukoYoshimura <101579747+YukoYoshimura@users.noreply.github.com>
2022-04-16 01:14:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 $(function () {
-    $("").click(function () {
+    $("#serch_btn").click(function () {
         // 入力された値を取得
-        const zipcode = $("#zipcode").val();
-        console.log(zipcode);
+        const inputValue = $("#zipcode").val();
         // urlを設定
         var url = "https://zipcloud.ibsnet.co.jp/api/search";
         // 送るデータを成形する
         var param = { zipcode: $("#zipcode").val() };
         // サーバーと通信(Ajax)
-        
         $.ajax({
             type: "GET", 
             cache: false,
             data: param,
-            url: "index.html",
+            url: url,
             dataType: "jsonp"
         })
         .done(function (res) {
@@ -27,12 +25,16 @@ $(function () {
                 for (var i = 0; i < res.results.length; i++) {
                     var result = res.results[i];
                     console.log(res.results);
-                    html += '<p>都道府県コード：' + result.prefcode + '</p>';
-
+                    html += "<div>都道府県コード：" + result.prefcode + "</div>";
+                    html += "<div>都道府県：" + result.address1 + "</div>";
+                    html += "<div>市区町村：" + result.address2 + "</div>";
+                    html += "<div>町域：" + result.address3 + "</div>";
+                    html += "<div>都道府県(カナ)：" + result.kana1 + "</div>";
+                    html += "<div>市区町村(カナ)：" + result.kana2 + "</div>";
+                    html += "<div>町域(カナ)：" + result.kana3 + "</div>";
                     $("#zip_result").html(html);
                 }
             }
-
         })
         .fail(function (error) {
             console.log(error);
